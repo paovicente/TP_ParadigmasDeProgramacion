d9adebaa7bb559c7ccd313c904f6564257344dc3
Paola Vicente <85862179+paovicente@users.noreply.github.com>
2026-04-28 13:43:01 -0300

    Update Enemy.cs

--- EngineGDI - Base.v2.4/EngineGDI/Enemy.cs
+++ EngineGDI - Base.v2.4/EngineGDI/Enemy.cs
@@ -3,67 +3,6 @@ using System;
 
 namespace EngineGDI
 {
-    /*public class Enemy
-    {
-        private Vector2 pos;
-        private Vector2 size = new Vector2(16, 16);
-        private Movement movement;
-        private string sprite;
-        private Vector2 direction;
-        private bool isActive = true;
-        
-        private static Random rng = new Random();
-
-        public Vector2 Pos => pos;
-        public Vector2 Size => size;
-        public string Sprite => sprite;
-        public bool IsActive => isActive;
-
-        public Enemy(string sprite, Vector2 startPos)
-        {
-            this.sprite = sprite;
-            this.pos = startPos;
-
-            movement = new Movement(120f);
-
-            PickRandomDirection();
-
-            isActive = true;
-        }
-
-        public void Update(float deltaTime)
-        {
-            movement.Move(ref pos, direction, deltaTime);
-
-            KeepInsideScreen();
-
-            /*if (pos.Y > Program.SCREEN_HEIGHT)
-            {
-                isActive = false;
-            }
-        }
-
-        private void PickRandomDirection()
-        {
-            //returns a number between -1 and 1
-            float x = (float)(rng.NextDouble() * 2 - 1);
-            float y = (float)(rng.NextDouble() * 2 - 1);
-
-            direction = new Vector2(x, y).Normalize();
-        }
-
-        private void KeepInsideScreen()
-        {
-            //simple bounce - basic enemy
-            if (pos.X < 0 || pos.X > Program.SCREEN_WIDTH) direction.X *= -1;
-            if (pos.Y < 0 || pos.Y > Program.SCREEN_HEIGHT) direction.Y *= -1;
-        }
-
-        public void Deactivate()
-        {
-            isActive = false;
-        }
-    }*/
     public abstract class Enemy
     {
         protected Vector2 pos;
@@ -76,13 +15,9 @@ namespace EngineGDI
         public string Sprite => sprite;
         public bool IsActive => isActive;
 
-        /// <summary>Puntos que suma el jugador al eliminar este enemigo con un proyectil.</summary>
+        /// <summary>Points earned by the player when he kills this enemy.</summary>
         public virtual int PointsOnKill => 0;
-
-        /// <summary>
-        /// Escala visual al dibujar el sprite. Por defecto coincide con el tamaño lógico (size),
-        /// pero se puede overridear para pixelart sin afectar la hitbox/mecánicas.
-        /// </summary>
+        
         public virtual Vector2 RenderScale => size;
 
         public Enemy(string sprite, Vector2 startPos)
